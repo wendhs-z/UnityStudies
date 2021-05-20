37047b6f57ab66dbf4402ba5f0d0bc2b80616f5f
Wendy Anjanette <78264583+wendhs-z@users.noreply.github.com>
2021-05-20 20:20:41 +0800

    Player is moving now.

---
+++ ObstacleStudy/Assets/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * 10f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * 10f;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
